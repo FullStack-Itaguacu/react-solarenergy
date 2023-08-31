e6e23bc86820f34b98fd3310ff539e3582fc7dbe
Erico Couto Jr <erico.coutjr@gmail.com>
2023-08-31 13:07:42 -0300

    fix: correção do path das imagens (icons)

--- src/components/menu-lateral/menu-lateral.jsx
+++ src/components/menu-lateral/menu-lateral.jsx
@@ -11,19 +11,19 @@ export const MenuLateral = () => {
   return (
     <div className="box">
       <div className="side-menu">
-        <img className="logo" alt="Logo" src="src\components\menu-lateral\imagens\logo-menu-bg-branco.png" />
+        <img className="logo" alt="Logo" src="src/components/menu-lateral/imagens/logo-menu-bg-branco.png" />
         <div className="list">
           <div className={`dashboard menu-item ${activeItem === "dashboard" ? "active" : ""}`} 
             onClick={() => handleItemClick("dashboard")}>
-            <img className="icone" alt="Icone" src={`src\components\menu-lateral\imagens\icone-dashboard-${activeItem === "dashboard" ? "branco" : "cinza"}.png`}/>
+            <img className="icone" alt="Icone" src={`src/components/menu-lateral/imagens/icone-dashboard-${activeItem === "dashboard" ? "branco" : "cinza"}.png`}/>
             <div className={`text ${activeItem === "dashboard" ? "active" : ""}`}>Dashboard</div>
           </div>
           <div className={`unidade-geradora menu-item ${activeItem === "unidade-geradora" ? "active" : ""}`} onClick={() => handleItemClick("unidade-geradora")}>
-            <img className="icone" alt="Icone" src={`src\components\menu-lateral\imagens\icone-cadastrar-${activeItem === "dashboard" ? "branco" : "cinza"}.png`} />
+            <img className="icone" alt="Icone" src={`src/components/menu-lateral/imagens/icone-cadastrar-${activeItem === "unidade-geradora" ? "branco" : "cinza"}.png`} />
             <div className={`text ${activeItem === "unidade-geradora" ? "active" : ""}`}>Unidades</div>
           </div>
           <div className={`consumo menu-item ${activeItem === "consumo" ? "active" : ""}`} onClick={() => handleItemClick("consumo")}>
-            <img className="icone" alt="Icone" src={`src/componentes/manu-lateral/imagens/icone-unidades-${activeItem === "dashboard" ? "branco" : "cinza"}.png`} />
+            <img className="icone" alt="Icone" src={`src/components/menu-lateral/imagens/icone-unidades-${activeItem === "consumo" ? "branco" : "cinza"}.png`} />
             <div className={`text ${activeItem === "consumo" ? "active" : ""}`}>Cadastro de energia grada</div>
           </div>
         </div>
