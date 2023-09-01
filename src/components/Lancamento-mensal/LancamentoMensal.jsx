24cb3bd874cab9cac93e1d3da72a4b3e7f29796e
adrianovolter <adriano.j.volter@hotmail.com>
2023-09-01 09:31:01 -0300

    adicionado endpoint get e post e funçao submit

--- src/components/Lancamento-mensal/LancamentoMensal.jsx
+++ src/components/Lancamento-mensal/LancamentoMensal.jsx
@@ -1,42 +1,77 @@
-import { useState } from 'react';
-import './Lancamento-mensal.module.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Lancamento-mensal.module.css";
 
 export const LancamentoGeracaoMensal = () => {
+  const [unidades, setUnidades] = useState([]);
+  const [unidadeGeradora, setUnidadeGeradora] = useState("");
+  const [data, setData] = useState("");
+  const [total, setTotal] = useState("");
+  const lancamento = {
+    id_unidade: unidadeGeradora,
+    data: data,
+    total: total,
+  };
 
-  const [unidadeGeradora, setUnidadeGeradora] = useState('');
-  const [mesAno, setMesAno] = useState('');
-  const [totalGerado, setTotalGerado] = useState('');
-  const lancamento = {unidadeGeradora: unidadeGeradora,mesAno: mesAno,totalGerado: totalGerado};
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/unidades")
+      .then((response) => {
+        setUnidades(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:3000/lancamentos", lancamento)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
       <header>
         <h1>Lançamento de geração mensal</h1>
       </header>
-      
-      <div className='formulario'>
-        <form>
+
+      <div className="formulario">
+        <form onSubmit={handleSubmit}>
           <div className="unidade">
             <label htmlFor="">Unidade Geradora</label>
-            <select name="" id="">
-              {/* <option value="">Selecione</option> */}
+            <select
+              name=""
+              id=""
+              value={unidadeGeradora}
+              onChange={(e) => setUnidadeGeradora(e.target.value)}
+            >
+              {unidades.map((unidade) => (
+                <option key={unidade.id} value={unidade.id}>
+                  {unidade.apelido}
+                </option>
+              ))}
             </select>
-            
           </div>
-          <div className='data'>
+          <div className="data">
             <label htmlFor="">Mês/ano</label>
-            <input type="month" name="" id="" />
+            <input type="month" name="data" />
           </div>
-          <div className='total'>
+          <div className="total">
             <label htmlFor="">Total kw gerado</label>
-            <input type="number" name="" id="" />
+            <input type="number" name="kw" />
           </div>
-          <div className='botao'>
-            <button
-              type="submit"
-              className="btn btn-primary"
-
-            >Cadastro</button>
+          <div className="botao">
+            <button type="submit" className="btn btn-primary">
+              Cadastro
+            </button>
           </div>
         </form>
       </div>
