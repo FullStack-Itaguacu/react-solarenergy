8e64b4afd236f05208341993e79f2b7b34f79896
douglas <douglas.df1@gmail.com>
2023-09-01 14:39:24 -0300

    fix: logica para dados do grafico

--- src/components/grafico/LineChart.jsx
+++ src/components/grafico/LineChart.jsx
@@ -1,97 +1,109 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import {
-    Chart as ChartJS,
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Title,
+  Legend,
+  Tooltip,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import axios from "axios";
+
+export const LineChart = () => {
+  const [listaUnidades, setListaUnidades] = useState([]);
+  const [listaLancamentos, setListaLancamentos] = useState([]);
+
+  useEffect(() => {
+    buscaUnidades();
+    buscaListaLancamentos();
+  }, []);
+
+  // Faz a busca de informações no endpoint unidades
+  const buscaUnidades = () => {
+    axios
+      .get("http://localhost:3000/unidades")
+      .then((response) => setListaUnidades(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  // Faz a busca de informações no endpoint lancamentos
+  const buscaListaLancamentos = () => {
+    axios
+      .get("http://localhost:3000/lancamentos")
+      .then((response) => setListaLancamentos(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  // Lógica para cálculo dos dados do gráfico
+  const totalUnidades = listaUnidades.length;
+  const unidadesAtivas = listaUnidades.filter(
+    (unidade) => unidade.ativa == true
+  ).length;
+  const unidadesInativas = listaUnidades.filter(
+    (unidade) => unidade.ativa == false
+  ).length;
+
+  let somaLancamentos = {};
+  listaLancamentos.forEach((element) => {
+    if (somaLancamentos[element.data]) {
+      somaLancamentos[element.data] += element.total;
+    } else {
+      somaLancamentos[element.data] = element.total;
+    }
+  });
+  console.log(somaLancamentos);
+
+  // Criação do Gráfico
+  ChartJS.register(
     LineElement,
     CategoryScale,
     LinearScale,
     PointElement,
     Title,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
-import axios from 'axios';
+    Tooltip,
+    Legend
+  );
 
-export const LineChart = () => {
+  // Dados do gráfico
+  const labels = Object.keys(somaLancamentos).map((key) => key);
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Total gerado mensalmente",
+        data: Object.values(somaLancamentos).map((value) => value),
+        backgroundColor: "white",
+        borderColor: "aqua",
+        tension: 0.4,
+        fill: true,
+      },
+    ],
+  };
 
-    const [listaUnidades, setListaUnidades] = useState([]);
-    const [listaLancamentos, setListaLancamentos] = useState([]);
-
-    useEffect(() => {
-        buscaUnidades()
-        buscaListaLancamentos()
-    }, [])
-
-    // Faz a busca de informações no endpoint unidades
-    const buscaUnidades = () => {
-        axios.get('http://localhost:3000/unidades')
-            .then(response => setListaUnidades(response.data))
-            .catch(error => console.log(error))
-    }
-
-    // Faz a busca de informações no endpoint lancamentos
-    const buscaListaLancamentos = () => {
-        axios.get('http://localhost:3000/lancamentos')
-            .then(response => setListaLancamentos(response.data))
-            .catch(error => console.log(error))
-    }
-
-    // Lógica para cálculo dos dados do gráfico
-    const totalUnidades = listaUnidades.length
-    const unidadesAtivas = listaUnidades.filter(unidade => unidade.ativa == true).length
-    const unidadesInativas = listaUnidades.filter(unidade => unidade.ativa == false).length
-
-    let somaLancamentos = {}
-    somaLancamentos = listaLancamentos.map(lancamento => {
-
-    })
-
-    // Criação do Gráfico
-    ChartJS.register(
-        LineElement,
-        CategoryScale,
-        LinearScale,
-        PointElement,
-        Title,
-        Legend
-    )
-
-    // Dados do gráfico
-    const data = {
-        labels: ['Janeiro', 'Fevereiro', 'Março'],
-        datasets: [{
-            label: "Total de energia gerada mensalmente",
-            data: [180, 480, 250],
-            backgroundColor: 'white',
-            borderColor: 'aqua',
-            tension: 0.4,
-            fill: true
-        }]
-    }
-
-    // Configurações do gráfico
-    const options = {
-        plugins: {
-            legend: {
-                onClick: false,
-                labels: {
-                    boxWidth: 0
-                }
-            },
+  // Configurações do gráfico
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        onClick: false,
+        labels: {
+          boxWidth: 0,
         },
-        elements: {
-            point: {
-                radius: 0
-            }
-        },
-        scales: {
-            y: {
-                min: 0,
-                max: 500
-            }
-        }
-    }
+      },
+    },
+    title: {
+      display: true,
+      text: "Total de energia gerada por mês",
+    },
+    scales: {
+      y: {
+        min: 0,
+      },
+    },
+  };
 
-    return (
-        <Line options={options} data={data} />
-    )
-}
+  return <Line options={options} data={data} />;
+};
