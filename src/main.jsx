60167e12872a6b879df7184f0ed681ab20867e6f
ricardo-werner <wernerdive@gmail.com>
2023-09-01 15:24:09 -0300

    fix: removido o import do index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './App'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap/dist/css/bootstrap.min.css';
