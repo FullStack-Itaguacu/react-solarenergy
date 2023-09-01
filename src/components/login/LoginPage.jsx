dae42957cd24b3e07fc6fdf991f48754198b9bf1
ricardo-werner <wernerdive@gmail.com>
2023-09-01 15:25:01 -0300

    style: configuracao do css = figma

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,6 +1,67 @@
-import { FiInbox, FiLock } from "react-icons/fi";
+//import { FiInbox, FiLock } from "react-icons/fi";
 
-const LoginPage = () => {
+export default function Login() {
+    return (
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="row border p-3 bg-white box-area">
+                <div className="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
+                    <div className="featured-img">
+                        <img
+                            className="img-fluid"
+                            src="./src/assets/LoginImagem/imagemeolicasolar.png"
+                            alt="imagem de um telhado com sistema de painel solar e torre de energia eolica"
+                        />
+                    </div>
+                </div>
+
+                <div className="col-md-6 right-box">
+                    <div className="row align-items-center">
+                        <div className="col-md-12 d-flex justify-content-center align-items-center">
+                            <img
+                                id="logo"
+                                src="./src/assets/LoginImagem/logo.png"
+                                alt="logo solar energy"
+                            />
+                        </div>
+                        <div className="header-text text-center">
+                            <p>Seja bem vindo</p>
+                        </div>
+                        <div className="input-container">
+                            <span className="input-icon">
+                                <img className="imagem-email"
+                                    src="./src/assets/LoginImagem/email.png"
+                                />
+                            </span>
+                            <input
+                                id="input"
+                                type="text"
+                                className="form-control"
+                                placeholder="E-mail"
+                            />
+                        </div>
+                        <div className="input-group">
+                            <span className="input-icon">
+                                <img className="imagem-senha"
+                                    src="./src/assets/LoginImagem/senha.png"
+                                />
+                            </span>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Senha"
+                            />
+                        </div>
+                        <div className="d-grid gap-2 col-4 mt-2 mx-auto" id="btn">
+                            <input id="btn" className="btn" value="Entrar" type="submit" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+/*export default function Login() {
     <div className="container d-flex justify-content-center align-items-center min-vh-100">
         <div className="row border rounded-5 p-3 bg-white shadow-none box-area">
             <div className="col-md-6 d-flex justify-content-center align-items-center flex-column left-box">
@@ -63,6 +124,4 @@ const LoginPage = () => {
             </div>
         </div>
     </div>
-}
-
-export default LoginPage;
+}*/
