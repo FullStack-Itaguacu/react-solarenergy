2d56ebc0e973f73181100af6ff4d6a0e71e59bd2
ricardo-werner <wernerdive@gmail.com>
2023-09-03 23:34:18 -0300

    style: alterado os inputs e inputs-icons

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,8 +1,23 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../../contexts/Auth";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 export default function Login() {
+    const { authenticated, login } = useContext(AuthContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit", { email, password });
+        login(email, password); //intregração com o meu contexto / consequentemente com a minha api
+    };
+
+
+
     return (
         <div className="container d-flex justify-content-center align-items-center min-vh-100">
             <div className="row rounded-3">
@@ -10,6 +25,7 @@ export default function Login() {
                     <div className="featured-img">
                         <img src="../src/assets/LoginImagem/imagemeolicasolar.png  " alt="imagem de um sistema solar com um sistema eolico" />
                     </div>
+                    <p>{authenticated}</p>
                 </div>
 
                 <div className="col-md-6 d-flex justify-content-center align-items-center right-box">
@@ -23,25 +39,33 @@ export default function Login() {
                         </div>
                         <div className="header-text mb-4 text-center">
                             <p>Seja bem vindo</p>
+                            <p>{String( authenticated )}</p>
                         </div>
-                        <div className="input-container">
+                        <form className="input-container" onSubmit={handleSubmit}>
                             <FontAwesomeIcon icon={faEnvelope} className="input-icon" />
                             <input
                                 id="input"
                                 type="text"
-                                className="form-control form-control-lg bg-light fs-6"
+                                className="form-control mb-3"
                                 placeholder="E-mail"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
-                        </div>
-                        <div className="input-container">
-                            <FontAwesomeIcon icon={faLock} className="input-icon" />
-                            <input
-                                id="input"
-                                type="password"
-                                className="form-control form-control-lg bg-light fs-6"
-                                placeholder="Senha"
-                            />
-                        </div>
+
+                            <div className="input-container">
+                                <FontAwesomeIcon icon={faLock} className="input-icon" />
+                                <input
+                                    id="input"
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Senha"
+                                    required
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                        </form>
                         <div className="d-grid gap-2 col-4 mt-2 mx-auto" id="btn">
                             <input id="btn" className="btn" value="Entrar" type="submit" />
                         </div>
