af09b3d9773f67628796cba7ba6f8bfcbe995d5c
ricardo-werner <wernerdive@gmail.com>
2023-09-05 10:27:18 -0300

    style: retirado o import do LoginPage.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/components/login/LoginPage.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
