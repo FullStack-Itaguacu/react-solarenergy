dec9ba6a36312668af81d027bc253a38973ae200
ricardo-werner <wernerdive@gmail.com>
2023-09-05 19:23:24 -0300

    refactor: del LoginPage.css e Bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/components/login/LoginPage.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
