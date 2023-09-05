4fcdc25db2300269e6f7d8d0b1b0b3b787f71154
deborasous <deborapatriciadp27@gmail.com>
2023-09-04 21:25:31 -0300

    Estilização do css do cadastroUnidade

--- src/components/cadastroUnidade/Cadastro.jsx
+++ src/components/cadastroUnidade/Cadastro.jsx
@@ -1,46 +1,55 @@
-import { useState } from "react";
-import axios from "axios";
-import { Form } from "../form/Form";
-import "./Cadastro.css";
-import PropTypes from "prop-types";
+import { useState } from 'react';
+import axios from 'axios';
+import { Form } from '../form/Form';
+import './Cadastro.css';
+import PropTypes from 'prop-types';
 
 export const CadastroUnidade = ({ mudarFormulario }) => {
   const [error, setError] = useState(false);
 
   const unidadeFields = [
     {
-      type: "text",
-      label: "Apelido",
-      name: "apelido",
-      placeholder: "Painel 1",
-      style: { width: "282px" },
+      type: 'text',
+      label: 'Apelido',
+      name: 'apelido',
+      placeholder: 'Painel 1',
+      inputClassName: 'input-pequeno',
     },
     {
-      type: "text",
-      label: "Local",
-      name: "local",
-      placeholder: "Rua Alberto 430",
+      type: 'text',
+      label: 'Local',
+      name: 'local',
+      placeholder: 'Rua Alberto 430',
+      inputClassName: 'input-extra-grande',
     },
     {
-      type: "text",
-      label: "Marca",
-      name: "marca",
-      placeholder: "marca",
+      type: 'text',
+      label: 'Marca',
+      name: 'marca',
+      placeholder: 'marca',
+      inputClassName: 'input-extra-grande',
     },
     {
-      type: "text",
-      label: "Modelo",
-      name: "modelo",
-      placeholder: "155w",
+      type: 'text',
+      label: 'Modelo',
+      name: 'modelo',
+      placeholder: '155w',
+      inputClassName: 'input-extra-grande',
     },
-    { type: "checkbox", label: "Ativa", name: "ativa", placeholder: "check" },
+    {
+      type: 'checkbox',
+      label: 'Ativo',
+      name: 'ativa',
+      placeholder: 'check',
+      className: 'checkbox-label',
+    },
   ];
-  const ENDPOINT_UNIDADES = "http://localhost:3000/unidades";
+  const ENDPOINT_UNIDADES = 'http://localhost:3000/unidades';
 
   const validarInputs = (formData) => {
     const { apelido, local, marca, modelo } = formData;
     if (!apelido || !local || !marca || !modelo) {
-      alert("Preencha todos os campos");
+      alert('Preencha todos os campos');
       return false;
     }
     return true;
@@ -50,11 +59,11 @@ export const CadastroUnidade = ({ mudarFormulario }) => {
     try {
       const updatedResponse = await axios.post(ENDPOINT_UNIDADES, novaUnidade);
       if (updatedResponse.status === 201) {
-        alert("Unidade cadastrada com sucesso");
+        alert('Unidade cadastrada com sucesso');
         setError(false);
         mudarFormulario();
       } else {
-        alert("Erro ao cadastrar unidade");
+        alert('Erro ao cadastrar unidade');
         setError(true);
       }
     } catch (error) {
@@ -68,9 +77,9 @@ export const CadastroUnidade = ({ mudarFormulario }) => {
   };
 
   return (
-    <div>
+    <div className="content">
       {error && <p>Erro ao realizar lançamento.</p>}
-      <h2>Cadastro de Unidade Geradora</h2>
+      <h2 className="sub-titulo">Cadastro de Unidade Geradora</h2>
       <Form
         fields={unidadeFields}
         onSubmit={handleUnidadeSubmit}
